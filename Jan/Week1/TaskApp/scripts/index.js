b6fcb6b055176c367e23c982c2027a4e59d3da9e
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2025-01-04 11:39:12 +0300

    Update included:   - Created the add tasks form in the HTML code   - Added the functionality and logic that grabs the tasks input by the user and saves it to localstorage   - Created a simple .css file that will be used to toggle the classes showing and hiding sections of the page

--- Jan/Week1/TaskApp/scripts/index.js
+++ Jan/Week1/TaskApp/scripts/index.js
@@ -3,15 +3,39 @@ const pendingTasks = document.querySelector(".Pending-tasks");
 const doneTasks = document.querySelector(".done-tasks");
 const addTask = document.querySelector(".add-tasks");
 
+// Add Task form
+const addTasksPage = document.querySelector(".addTasksPage");
+const addTaskForm = document.querySelector(".addTaskForm");
+const titleInput = document.querySelector(".titleInput").value;
+const descriptionInput = document.querySelector(".descriptionInput").value;
+const dateInput = document.querySelector(".dateInput").value;
+const addTaskBtn = document.querySelector(".addTaskBtn");
+
 const getTasksFromLocalStorage = () => {
   let localTasks = localStorage.getItem("tasks");
   if(localTasks) {
     let tasks = JSON.parse(localTasks);
-    return tasks;
+    console.log(tasks);
   } else {
-    return "You currently do not have any tasks"
+    return "You currently do not have any tasks";
   }
 }
+
+addTaskBtn.addEventListener("click", () => {
+  let formData = {
+    id: 1,
+    title: titleInput,
+    description: descriptionInput,
+    deadline: dateInput,
+    status: false
+  }
+  localStorage.setItem("tasks", JSON.stringify(formData))
+})
+
+
+addTask.addEventListener("click", () => {
+  addTasksPage.classList.add("active");
+})
 
 window.addEventListener("load", () => 
 getTasksFromLocalStorage()
